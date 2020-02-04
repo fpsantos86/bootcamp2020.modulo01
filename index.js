9bc33d2a7fc906a35a3a9c928917b4e91a698884
unknown <fpsantos86@hotmail.com>
2020-02-04 05:55:49 -0300

    Exemplo utilizando Route params

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const server = express();
 // Route params = /users/1
 // Request body = { "name": "Diego", "email": "fpsantos86@hotmail.com" } 
 
-server.get('/teste',(req,res)=>{
-    const nome = req.query.nome;
+server.get('/users/:id',(req,res)=>{
+    const id = req.params.id;
 
-    return res.json({message:`Hello ${nome}`});
+    return res.json({message:`Buscando o usuário ${id}`});
 })
 
 server.listen(3000);
