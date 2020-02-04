47197cbdd3273cb6dbb4e401808537cacf6144f2
unknown <fpsantos86@hotmail.com>
2020-02-04 05:52:09 -0300

    Exemplo utilizando Query Params

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const server = express();
 
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = { "name": "Diego", "email": "fpsantos86@hotmail.com" } 
+
 server.get('/teste',(req,res)=>{
-    return res.json({message:'Hello World'});
+    const nome = req.query.nome;
+
+    return res.json({message:`Hello ${nome}`});
 })
 
 server.listen(3000);
