940eddc25a620a76f6b3e0008cfa3af82e408755
unknown <fpsantos86@hotmail.com>
2020-02-04 06:18:54 -0300

    Exemplo utilizando Request body - foi necessário implementar o código server.use(express.json()) para a requisição interpretar o body (corpo) como JSON

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "name": "Diego", "email": "fpsantos86@hotmail.com" } 
 
+// CRUD = Create, Read, Update, Delete
 
-const users = ['Diego', 'Cláudio', 'Victor'];
+const users = ['Diego', 'Robson', 'Victor'];
+
+server.get('/users', (req,res)=>{
+    return res.json(users);
+})
+
 
 server.get('/users/:index',(req,res)=>{
     const { index } = req.params;
 
     return res.json(users[index]);
+})
+
+server.post('/users', (req,res)=>{
+    const { name } = req.body;
+
+    users.push(name);
+
+    return res.json(users);
 })
 
 server.listen(3000);
