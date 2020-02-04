5b96c2bb50b1bea8b4af73379a07f10cf434e888
unknown <fpsantos86@hotmail.com>
2020-02-04 06:56:24 -0300

    Implementando um CRUD com os métodos HTTP post, get, put, delete - Utilizando Insomnia para testar as requisições

--- index.js
+++ index.js
@@ -29,7 +29,24 @@ server.post('/users', (req,res)=>{
     users.push(name);
 
     return res.json(users);
-})
+});
+
+server.put('/users/:index', (req,res)=>{
+    const { index } = req.params;
+    const{ name } = req.body;
+
+    users[index] = name;
+
+    return res.json(users);
+});
+
+server.delete('/users/:index', (req,res)=>{
+    const { index } = req.params;
+  
+    users.splice(index, 1);
+
+    return res.send();
+});
 
 server.listen(3000);
 
